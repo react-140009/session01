e0a238f5fe243f2c64dee41ad407948a206ac4aa
pasha <pashaie@gmail.com>
2021-11-23 21:21:26 +0330

    components/TodoItem:

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export function TodoItem(props) {
+  const { todo } = props;
   return (
     <tr>
-      <td>{props.todo.id}</td>
-      <td>{props.todo.title}</td>
+      <td>{todo.id}</td>
+      <td>{todo.title}</td>
       <td>del</td>
     </tr>
   );
